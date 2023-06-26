e45e3e9d57442297eb17468a425a12323731d724
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-06-26 22:40:30 +0200

    Make createGrid function work

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const gridContainer = document.querySelector(".grid-container");
 
 function createGrid(size) {
+  gridContainer.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("grid-item");
     gridContainer.appendChild(square);
   }
-
-
 }
 
-createGrid(50);
+createGrid(16);
