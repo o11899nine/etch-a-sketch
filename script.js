3995359ca2ccf3a1b85ed0de937a8d315cf82624
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-06-26 21:24:54 +0200

    Start createGrid function

---
+++ script.js
@@ -0,0 +1,22 @@
+const grid = document.querySelector(".grid");
+const gridWidth = grid.offsetWidth;
+
+function createGrid(size) {
+  const squareSize = gridWidth / size; 
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    grid.appendChild(row);
+    for (let j = 0; j < size; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.style.height = `${squareSize}px`;
+      square.style.width = `${squareSize}px`;
+      row.appendChild(square);
+    }
+  }
+  
+  
+}
+
+createGrid(20);
