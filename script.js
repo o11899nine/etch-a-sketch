ed774116e6c15a4b037592db8fbf0be6e7181b5c
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-06-28 15:58:16 +0200

    Create random color function

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 const gridContainer = document.querySelector(".grid-container");
+const randomColorBtn = document.querySelector(".random-color-btn")
 
 let mouseDown = false;
+let selectedColor = "rgb(0,0,0)";
+let randomizer = false;
 
 document.addEventListener('mousedown', () => {
   mouseDown = true;
-})
+});
 
 document.addEventListener('mouseup', () => {
   mouseDown = false;
-})
+});
+
+function randInt(number) {
+  return Math.floor(Math.random() * number);
+}
+
+function randomColor() {
+  return `rgb(${randInt(255)}, ${randInt(255)}, ${randInt(255)})`;
+}
+
+randomColorBtn.addEventListener('click', () => {
+  randomizer = true;
+});
+
+function paintSquare(e) {
+  if (mouseDown) {
+    if (randomizer) {
+      e.target.style.backgroundColor = randomColor();
+    } else {
+      e.target.style.backgroundColor = selectedColor;
+    }
+  }
+}
 
 function createGrid(size) {
   gridContainer.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
-    square.classList.add("grid-item");
+    square.classList.add("square");
+    square.addEventListener('mouseover', paintSquare);
     gridContainer.appendChild(square);
   }
 }
 
 
-let paintColor = "black";
 
-function paintSquare(e) {
-  if (mouseDown) {
-    e.target.style.backgroundColor = "black";
-  }
-}
+
 
 createGrid(50);
-
-
-
-const squares = document.querySelectorAll(".grid-item");
-squares.forEach((square) => {
-  square.addEventListener('mouseover', paintSquare);
-});
