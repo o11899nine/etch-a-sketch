e0f64ceb1be60e2f1cb9a455bd0791c7e0cc712b
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-06-28 21:45:19 +0200

    Cleanup JS code

--- script.js
+++ script.js
@@ -1,62 +1,59 @@
-const gridContainer = document.querySelector(".grid-container");
-const randomColorBtn = document.querySelector(".random-color-btn");
-const eraserBtn = document.querySelector(".eraser-btn");
-const paintColorPicker = document.querySelector(".paint-color-picker");
 const backgroundColorPicker = document.querySelector(".background-color-picker");
+const eraserBtn = document.querySelector(".eraser-btn");
+const gridContainer = document.querySelector(".grid-container");
+const paintColorPicker = document.querySelector(".paint-color-picker");
+const randomColorBtn = document.querySelector(".random-color-btn");
+
+backgroundColorPicker.addEventListener("input", changeBackgroundColor);
+eraserBtn.addEventListener("click", activateEraser);
+paintColorPicker.addEventListener("input", changePaintColor);
+randomColorBtn.addEventListener("click", () => { randomColor = true });
 
 let currentBGColor = backgroundColorPicker.value;
 let currentPaintColor = paintColorPicker.value;
+let mouseDown = false;
+let randomColor = false;
 
+document.addEventListener("mousedown", () => { mouseDown = true });
+document.addEventListener("mouseup", () => { mouseDown = false });
+
+// Helper functions
 function rgbToHex(rgb) {
   return `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`;
 }
 
-paintColorPicker.addEventListener("input", () => {
-  randomColor = false;
-  currentPaintColor = paintColorPicker.value;
-});
+function getRandomHexColor() {
+  const hexColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${hexColor}`;
+}
 
-backgroundColorPicker.addEventListener("input", changeBackgroundColor);
+// Main functions
+function activateEraser() {
+  randomColor = false;
+  currentPaintColor = currentBGColor;
+}
+
+function changePaintColor() {
+  randomColor = false;
+  currentPaintColor = this.value;
+}
 
 function changeBackgroundColor() {
   const newBGColor = this.value;
   const squares = document.querySelectorAll(".square");
+
   squares.forEach((square) => {
     const squareBGColor = rgbToHex(square.style.backgroundColor);
     if (squareBGColor === currentBGColor) {
       square.style.backgroundColor = newBGColor;
     }
   });
+  
+  currentBGColor = newBGColor;
 }
-
-
-
-let mouseDown = false;
-document.addEventListener("mousedown", () => { mouseDown = true });
-document.addEventListener("mouseup", () => { mouseDown = false });
-
-
-let randomColor = false;
-randomColorBtn.addEventListener("click", () => { randomColor = true });
-
-eraserBtn.addEventListener("click", activateEraser);
-
-
-function activateEraser() {
-  currentPaintColor = currentBGColor;
-  randomColor = false;
-}
-
-
-function getRandomColor() {
-  const hexColorValue = Math.floor(Math.random() * 16777215).toString(16);
-  return `#${hexColorValue}`;
-}
-
-
 
 function paintSquare(event) {
-  if (randomColor) { currentPaintColor = getRandomColor(); }
+  if (randomColor) { currentPaintColor = getRandomHexColor(); }
   if (event.type === "mouseover") {
     if (mouseDown) {
       this.style.backgroundColor = currentPaintColor;
@@ -69,6 +66,7 @@ function paintSquare(event) {
 
 function createGrid(size) {
   gridContainer.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+  
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
@@ -78,9 +76,5 @@ function createGrid(size) {
     gridContainer.appendChild(square);
   }
 }
-
-
-
-
 
 createGrid(16);
